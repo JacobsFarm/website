bb8de3ed5885a1e817cc8cb70057b4ccf3169120
Jacobs Farm <jacobsfarmextras@gmail.com>
2026-03-10 23:20:21 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+    kit: { 
+        adapter: adapter({
+            // fallback: '404.html' zorgt dat routes ook werken na een refresh op GitHub Pages
+            fallback: '404.html' 
+        }),
+        paths: {
+            // Omdat je repo 'website' heet, gebruik je '/website'
+            base: process.env.NODE_ENV === 'production' ? '/website' : '',
+        }
+    },
+    preprocess: [mdsvex()],
+    extensions: ['.svelte', '.svx']
 };
 
 export default config;
