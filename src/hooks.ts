42920e688898382c65cb98e207672e65ed507787
Jacobs Farm <jacobsfarmextras@gmail.com>
2026-03-19 21:54:37 +0100

    Update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-import { deLocalizeUrl } from '$lib/paraglide/runtime';
-
-export const reroute = (request) => deLocalizeUrl(request.url).pathname;
-=======
 export const reroute = undefined;
 export const transport = undefined;
->>>>>>> 2c42776 (Lokale wijzigingen opslaan voor pull)
