cfa7ef1b755b13d4116ec7d3cade29ea54154fb9
Jacobs Farm <jacobsfarmextras@gmail.com>
2026-03-19 21:55:08 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,6 @@
 <script>
     import Navbar from '$lib/components/Navbar.svelte';
     import Footer from '$lib/components/Footer.svelte';
-<<<<<<< HEAD
-</script>
-
-<Navbar />
-
-<main>
-  <slot /> 
-=======
     import LanguageModal from '$lib/components/LanguageModal.svelte';
 </script>
 
@@ -18,31 +10,11 @@
 
 <main>
   <slot />
->>>>>>> 2c42776 (Lokale wijzigingen opslaan voor pull)
 </main>
 
 <Footer />
 
 <style>
-<<<<<<< HEAD
-  :global(body) { 
-    margin: 0; 
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
-    background-color: #f9fbf9; /* Zachte off-white achtergrond */
-    color: #2c3e2c;
-  }
-
-  /* De main selector werkt nu omdat er een <main> tag in de HTML staat */
-  main { 
-    max-width: 1100px; 
-    margin: 0 auto; 
-    padding: 3rem 2rem; 
-    min-height: 80vh; 
-  }
-
-  :global(h1) {
-    color: #386938; /* RAL 6001 Emerald Green */
-=======
   :global(body) {
     margin: 0;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
@@ -59,6 +31,5 @@
 
   :global(h1) {
     color: #386938;
->>>>>>> 2c42776 (Lokale wijzigingen opslaan voor pull)
   }
 </style>
